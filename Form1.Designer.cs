997b1c1c2ba3e82ef65fd2c3b702f7cf5f121973
cgamingpro <hariomjangra777@gmail.com>
2024-03-04 17:06:43 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace defintlyYes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NOBB = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NOBB
+            // 
+            this.NOBB.BackColor = System.Drawing.SystemColors.Info;
+            this.NOBB.Font = new System.Drawing.Font("Segoe Script", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NOBB.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.NOBB.Location = new System.Drawing.Point(301, 338);
+            this.NOBB.Name = "NOBB";
+            this.NOBB.Size = new System.Drawing.Size(96, 38);
+            this.NOBB.TabIndex = 0;
+            this.NOBB.Text = "NO";
+            this.NOBB.UseVisualStyleBackColor = false;
+            this.NOBB.MouseEnter += new System.EventHandler(this.NOBB_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(293, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Will you be Mine <:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Segoe Script", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Location = new System.Drawing.Point(509, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Yes";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(921, 594);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NOBB);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "will you be mine";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NOBB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace defintlyYes
+{
+    public partial class Form1 : Form
+    {
+        private Random rnd = new Random();
+        private Point[] POINTS = new Point[6];
+        public Form1()
+        {
+           
+            InitializeComponent();
+            BackColor = Color.LightPink;
+            inPoint();
+        }
+        private void inPoint()
+        {
+            POINTS[0] = new Point(678,380);
+            POINTS[1] = new Point(692,47);
+            POINTS[2] = new Point(409,35);
+            POINTS[3] = new Point(12,29);
+            POINTS[4] = new Point(12,217);
+            POINTS[5] = new Point(28,382);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Love you !!");
+        }
+
+        private void NOBB_MouseEnter(object sender, EventArgs e)
+        {
+            int i = rnd.Next(0,POINTS.Length);
+            Point locNew = POINTS[i];
+            NOBB.Location = locNew;
+        }
+    }
+}
